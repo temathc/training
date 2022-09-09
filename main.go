fccc9719ad63f8522a9540cf3ed0bba5e8371811
temathc <temathc@yandex.ru>
2022-09-09 22:50:33 +0300

    add check str

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ func main() {
 	fmt.Println("Hi")
 	ss := sum(4, 5)
 	fmt.Println("Sum =", ss)
+	s := "привет"
+	checkStr(s)
 }
 
 func sum(a int, b int) int {
 	return a + b
+}
+
+func checkStr(str string) {
+	for ind, val := range str {
+		fmt.Printf("Индекс = %d, значение = %s\n", ind, string(val))
+	}
 }
